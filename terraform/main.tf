02e5e59d4a1b66bd62ddc3642cef74e6669d682e
AppsysGlobal <sajith.m@appsysglobal.com>
2025-05-06 16:12:21 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,6 +9,7 @@ terraform {
 
 provider "oci" {
   tenancy_ocid     = var.tenancy_ocid
+  compartment_ocid = var.compartment_ocid
   user_ocid        = var.user_ocid
   fingerprint      = var.fingerprint
   private_key_path = var.private_key_path
