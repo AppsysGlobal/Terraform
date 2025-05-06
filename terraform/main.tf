6be2b031f8cb9732e43932722ea553de0d9b7ba1
AppsysGlobal <sajith.m@appsysglobal.com>
2025-05-06 17:09:36 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,6 +13,7 @@ provider "oci" {
   fingerprint      = var.fingerprint
   private_key_path = var.private_key_path
   region           = var.region
+  namespace        = var.namespace
 }
 
 # Existing Bucket import placeholder
