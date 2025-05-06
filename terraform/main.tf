de2263d9332703a4c148c1c2bcca2dfa3de0430f
AppsysGlobal <sajith.m@appsysglobal.com>
2025-05-06 17:20:03 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,11 +9,15 @@ terraform {
 
 provider "oci" {
   tenancy_ocid     = var.tenancy_ocid
+  compartment_ocid = var.compartment_ocid
   user_ocid        = var.user_ocid
   fingerprint      = var.fingerprint
   private_key_path = var.private_key_path
   region           = var.region
-}
+} 
+data "oci_objectstorage_namespace" "ns" {
+  compartment_id = var.compartment_ocid
+} 
 
 # Existing Bucket import placeholder
 resource "oci_objectstorage_bucket" "existing_bucket" {
