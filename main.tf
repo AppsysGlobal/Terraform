09af0dffbb317909728d33d84e9ead582175a103
AppsysGlobal <sajith.m@appsysglobal.com>
2025-05-09 15:30:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,3 @@
-
-
-
 provider "oci" {
   tenancy_ocid     = var.tenancy_ocid
   user_ocid        = var.user_ocid
@@ -8,18 +5,15 @@ provider "oci" {
   private_key_path = var.private_key_path
   region           = var.region
 }
-# Use existing VCN for instance creation
+
 resource "oci_core_instance" "test_instance" {
-  lifecycle {
-    ignore_changes = [metadata, create_vnic_details]  
-  }
   availability_domain = "Rnan:US-ASHBURN-AD-1"
   compartment_id      = var.compartment_ocid
-  shape = "VM.Standard.E2.1.Micro"
+  shape               = "VM.Standard.E2.1.Micro"
   display_name        = "Test-Instance"
 
   create_vnic_details {
-    subnet_id = "ocid1.subnet.oc1.iad.aaaaaaaaxeyq7yugxchxhh74vfl6tmfojvyuqpdy567svjpw3xh3ex6d2mwa"
+    subnet_id        = "ocid1.subnet.oc1.iad.aaaaaaaaxeyq7yugxchxhh74vfl6tmfojvyuqpdy567svjpw3xh3ex6d2mwa"
     assign_public_ip = true
     display_name     = "test-vnic"
     hostname_label   = "testvm"
@@ -27,13 +21,11 @@ resource "oci_core_instance" "test_instance" {
 
   metadata = {
     ssh_authorized_keys = file(var.ssh_public_key_path)
-    terraform_log = "Instance created with Terraform"
   }
 
   source_details {
     source_type = "image"
     source_id   = "ocid1.image.oc1.iad.aaaaaaaawewgudkwygjwoav2qymlfbbnhhesbqqp5gscroizizolah3bv6kq"
-    
   }
 }
 
@@ -48,4 +40,3 @@ data "oci_core_images" "oracle_linux_image" {
   sort_by                  = "TIMECREATED"
   sort_order               = "DESC"
 }
-
