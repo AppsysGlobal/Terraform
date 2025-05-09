5963c3c66fa2a49fda43c86c69f412ad53d10f5d
AppsysGlobal <sajith.m@appsysglobal.com>
2025-05-09 15:27:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,13 @@
 
 
+
+provider "oci" {
+  tenancy_ocid     = var.tenancy_ocid
+  user_ocid        = var.user_ocid
+  fingerprint      = var.fingerprint
+  private_key_path = var.private_key_path
+  region           = var.region
+}
 # Use existing VCN for instance creation
 resource "oci_core_instance" "test_instance" {
   lifecycle {
